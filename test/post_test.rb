0bbada14572f7349e13de82ca9c16bbc8072d4b0
Jennifer <jennifersoden@hotmail.com>
2016-04-13 14:48:02 -0600

    fixed stuff

--- test/post_test.rb
+++ test/post_test.rb
@@ -5,11 +5,12 @@ require './lib/post'
 class PostTest < Minitest::Test
 
   def test_it_creates_a_new_post
-    assert File.exist?(File.join(Dir.home << "/my-blog/source/posts/new_post.markdown")), FileMaker.new.create_files("/my-blog/source/posts/new_post.markdown", "* content in the form of a list!")
+    FileMaker.new.create_files("/my-blog/source/posts/new_post.markdown", "* content in the form of a list!")
+    assert File.exist?(File.join(Dir.home << "/my-blog/source/posts/new_post.markdown"))
   end
 
   def test_it_creates_a_new_post_with_body
-    assert File.exist?(File.join(Dir.home << "/my-blog/source/posts/new_post.markdown")), Post.new.post("/my-blog", "title")
+    Post.new.post("my-blog", "title")
+    assert File.exist?(File.join(Dir.home << "/my-blog/source/posts/new_post.markdown"))
   end
-
 end
