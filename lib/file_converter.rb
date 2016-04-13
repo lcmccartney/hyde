bb67e7926e8c5b18d45a043d5439c584f65bfb4c
Jennifer <jennifersoden@hotmail.com>
2016-04-12 18:01:58 -0600

    getting filestuff done

--- lib/file_converter.rb
+++ lib/file_converter.rb
@@ -12,7 +12,11 @@ class FileConverter
       # from spec: Kramdown::Document.new(markdown).to_html
       #use kramdown to create converted copies into the _output
       #need to extract contents from each file
+      new_path = file.sub("source", "_output")
+      require 'pry' ; binding.pry
+      content = File.open(file)
       html = Kramdown::Document.new(content).to_html
+      File.new(html, new_path)
       #put html into a file in the _output corresponding to source
     end
   end
--- test/file_converter_test.rb
+++ test/file_converter_test.rb
@@ -9,11 +9,11 @@ class FileConverterTest < Minitest::Test
   end
 
   def test_it_can_find_markdown_files
-    assert File.exist?("*.md"), @file_converter.find_markdown("my-blog")
+    assert_equal ["/Users/JenniferSoden/my-blog/source/index.markdown", "/Users/JenniferSoden/my-blog/source/pages/about.markdown", "/Users/JenniferSoden/my-blog/source/posts/date-welcome-to-hyde.markdown"], @file_converter.find_markdown("my-blog")
   end
 
   def test_it_can_use_kramdown_to_convert_to_html
-    assert File.exist?("*.html"), @file_converter.markdown_to_html("my-blog")
+    assert @file_converter.markdown_to_html("my-blog")
   end
 
 end
