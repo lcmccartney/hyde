74252879daf9341649df12fdab5b080538ef6946
Jennifer <jennifersoden@hotmail.com>
2016-04-14 22:58:47 -0600

    uncommenting

--- Gemfile
+++ Gemfile
@@ -6,6 +6,5 @@ gem "kramdown"
 gem 'cane'
 gem 'reek'
 gem 'rake'
-# gem 'listen', '~> 3.0'
-
+gem 'listen', '~> 3.0'
 gem 'simplecov', :require => false, :group => :test
--- lib/watch_fs.rb
+++ lib/watch_fs.rb
@@ -3,10 +3,8 @@ require 'listen'
 class WatchFs
 
   def start_listening(path)
-    listener = Listen.to("~/blog/source") do |modified, added, removed|
+    listener = Listen.to("~/blog/source") do |modified|
       puts "modified absolute path: #{modified}"
-      puts "added absolute path: #{added}"
-      puts "removed absolute path: #{removed}"
     end
     listener.start
     sleep
