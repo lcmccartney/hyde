079c1b52c1f3af510e351d8e963c6d8a7dbfc08d
Jennifer <jennifersoden@hotmail.com>
2016-04-15 08:55:02 -0600

    filewatcher works

--- lib/watch_fs.rb
+++ lib/watch_fs.rb
@@ -3,11 +3,18 @@ require 'listen'
 class WatchFs
 
   def start_listening(path)
-    listener = Listen.to("~/blog/source") do |modified|
+    listener = Listen.to("#{Dir.home}/#{path}/source") do |modified, _added, _removed|
       puts "modified absolute path: #{modified}"
+      build(path)
     end
     listener.start
     sleep
+  end
+
+  def build(path)
+    fileconverter = FileConverter.new
+    fileconverter.markdown_to_html(path)
+    fileconverter.md_to_html(path)
   end
 
 end
