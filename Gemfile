a2aa9cb423caa0d8fe8e9003b507bcf4f49421d5
Jennifer <jennifersoden@hotmail.com>
2016-04-14 22:43:36 -0600

    trying to file_watcher

--- Gemfile
+++ Gemfile
@@ -6,4 +6,5 @@ gem "kramdown"
 gem 'cane'
 gem 'reek'
 gem 'rake'
+gem 'listen', '~> 3.0'
 gem 'simplecov', :require => false, :group => :test
---
+++ lib/watch_fs.rb
@@ -0,0 +1,15 @@
+require 'listen'
+
+class WatchFs
+
+  def start_listening(path)
+    listener = Listen.to("/#{path}/source") do |modified, added, removed|
+      puts "modified absolute path: #{modified}"
+      puts "added absolute path: #{added}"
+      puts "removed absolute path: #{removed}"
+    end
+    listener.start
+    sleep
+  end
+
+end
